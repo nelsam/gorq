99376741d733284ce8cd93c0778d67faa1a93789
Samuel Nelson <valczir.darkvein@gmail.com>
2015-02-01 12:01:35 -0700

    Added a JSONObjectField SqlWrapper for querying postgres's json data type

--- extensions/postgres_filters.go
+++ extensions/postgres_filters.go
@@ -1,6 +1,9 @@
 package extensions
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/nelsam/gorq/filters"
 )
 
@@ -10,4 +13,25 @@ func ILike(fieldPtr interface{}, pattern string) filters.Filter {
 		Comparison: " ilike ",
 		Right:      pattern,
 	}
+}
+
+type jsonObjectFieldWrapper struct {
+	actualValue interface{}
+	fields      []string
+}
+
+func (wrapper jsonObjectFieldWrapper) ActualValue() interface{} {
+	return wrapper.actualValue
+}
+
+func (wrapper jsonObjectFieldWrapper) WrapSql(sqlValue string) string {
+	objElementSelector := strings.Join(wrapper.fields, ",")
+	return fmt.Sprintf("%s::json#>>'{%s}'", sqlValue, objElementSelector)
+}
+
+// JSONObjectField returns a filters.SqlWrapper that wraps the passed in value
+// in a JSON operator to query against elements in the json field.  It performs
+// a type cast to json, so columns which haven't been defined as json will work.
+func JSONObjectField(value interface{}, fields ...string) filters.SqlWrapper {
+	return jsonObjectFieldWrapper{actualValue: value, fields: fields}
 }
