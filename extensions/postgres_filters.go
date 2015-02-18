8c2eda0383c2c36c068c5bfc50a415ee5dd57ced
Samuel Nelson <valczir.darkvein@gmail.com>
2015-02-18 12:40:44 -0700

    Allow both json data queries (field#>something) as well as json text queries

--- extensions/postgres_filters.go
+++ extensions/postgres_filters.go
@@ -15,6 +15,20 @@ func ILike(fieldPtr interface{}, pattern string) filters.Filter {
 	}
 }
 
+type jsonObjectFieldTextWrapper struct {
+	actualValue interface{}
+	fields      []string
+}
+
+func (wrapper jsonObjectFieldTextWrapper) ActualValue() interface{} {
+	return wrapper.actualValue
+}
+
+func (wrapper jsonObjectFieldTextWrapper) WrapSql(sqlValue string) string {
+	objElementSelector := strings.Join(wrapper.fields, ",")
+	return fmt.Sprintf("(%s::json#>>'{%s}')", sqlValue, objElementSelector)
+}
+
 type jsonObjectFieldWrapper struct {
 	actualValue interface{}
 	fields      []string
@@ -26,12 +40,22 @@ func (wrapper jsonObjectFieldWrapper) ActualValue() interface{} {
 
 func (wrapper jsonObjectFieldWrapper) WrapSql(sqlValue string) string {
 	objElementSelector := strings.Join(wrapper.fields, ",")
-	return fmt.Sprintf("%s::json#>>'{%s}'", sqlValue, objElementSelector)
+	return fmt.Sprintf("(%s::json#>'{%s}')", sqlValue, objElementSelector)
 }
 
-// JSONObjectField returns a filters.SqlWrapper that wraps the passed in value
-// in a JSON operator to query against elements in the json field.  It performs
-// a type cast to json, so columns which haven't been defined as json will work.
+// JSONObjectFieldText returns a filters.SqlWrapper that wraps the
+// passed in value in a JSON operator to query against elements
+// (converted to text) in the json field.  It performs a type cast of
+// the field to json, so columns which haven't been defined as json
+// will work.
+func JSONObjectFieldText(value interface{}, fields ...string) filters.SqlWrapper {
+	return jsonObjectFieldTextWrapper{actualValue: value, fields: fields}
+}
+
+// JSONObjectField returns a filters.SqlWrapper that wraps the passed
+// in value in a JSON operator to query against elements in the json
+// field.  It performs a type cast of the field to json, so columns
+// which haven't been defined as json will work.
 func JSONObjectField(value interface{}, fields ...string) filters.SqlWrapper {
 	return jsonObjectFieldWrapper{actualValue: value, fields: fields}
 }
