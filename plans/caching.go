3663e70c56d46e24c153fd8c888bef8ad4b4e10e
Tyler Bunnell <tylerbunnell@gmail.com>
2015-05-14 10:41:12 -0600

    Custom encoder for caching

--- plans/caching.go
+++ plans/caching.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"compress/gzip"
 	"encoding/json"
+	"reflect"
 	"strings"
 
 	"github.com/memcachier/mc"
@@ -17,7 +18,7 @@ const defaultCacheExpirationTime = 0 // never expire
 func prepareForCache(data interface{}) (string, error) {
 	var b bytes.Buffer
 	w := gzip.NewWriter(&b)
-	err := json.NewEncoder(w).Encode(data)
+	err := json.NewEncoder(w).Encode(convertToMemcacheVal(data))
 	if err != nil {
 		w.Close()
 		return "", err
@@ -67,4 +68,44 @@ func getCacheData(cacheKey string, cache *mc.Conn) ([]interface{}, error) {
 		return nil, err
 	}
 	return restoreFromCache(s)
+}
+
+func convertToMemcacheVal(data interface{}) interface{} {
+	val := reflect.ValueOf(data)
+	if val.Kind() == reflect.Ptr {
+		if val.IsNil() {
+			return nil
+		}
+		// indirect the pointer to handle it transparently
+		val = val.Elem()
+	}
+
+	switch val.Kind() {
+	case reflect.Slice, reflect.Array:
+		res := make([]interface{}, 0, val.Len())
+		for i := 0; i < val.Len(); i++ {
+			elem := val.Index(i)
+			res = append(res, convertToMemcacheVal(elem.Interface()))
+		}
+		return res
+	case reflect.Struct:
+		res := make(map[string]interface{})
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Field(i)
+			fieldInfo := val.Type().Field(i)
+			name := fieldInfo.Tag.Get("db")
+			if idx := strings.IndexRune(name, ','); idx >= 0 {
+				name = name[:idx]
+			}
+			res[name] = convertToMemcacheVal(field.Interface())
+		}
+		return res
+	case reflect.Map:
+		res := make(map[string]interface{})
+		keys := val.MapKeys()
+		for _, key := range keys {
+			res[key.String()] = convertToMemcacheVal(val.MapIndex(key))
+		}
+	}
+	return data
 }
